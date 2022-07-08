73829b3a96fcc62bb755ceac27d5d64671541be2
dtamta007 <dtamta007@gmail.com>
2022-07-08 14:02:16 +0530

    Deployment

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,9 +1,9 @@
-import {axiosInstance} from "./config";
+import axios from 'axios';
 
 export const loginCall = async (user,dispatch) =>{
     dispatch({type: "LOGIN_START"});
     try{
-        const res = await axiosInstance.post("/auth/login", user);
+        const res = await axios.post("https://dtamtasocialmedia.herokuapp.com/api/auth/login", user);
         console.log("RES DATA: ", res.data);
         dispatch({type: "LOGIN_SUCCESS", payload: res.data});
     }catch(err){
